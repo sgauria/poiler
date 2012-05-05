5f60db9edd1a6960bf589ae8f1c916b52db73d46
Sameer Gauria (totopos) <sameergauria@yahoo.com>
2012-05-05 09:44:22 -0700

    39

---
+++ 39.py
@@ -0,0 +1,28 @@
+import math
+import time
+
+c1 = time.clock()
+N = 1000
+
+maxcount = 0
+maxp     = 0
+for p in range(1,N+1):
+    s = math.ceil(p/2)
+    count = 0
+    for a in range(1,s):
+        for b in range(a,s):
+            c = p - a - b
+            if (b < c):
+                if (c < s):
+                    if (a*a + b*b == c*c):
+                        count += 1
+                        #print (p,a,b,c)
+            else :
+                break # c is less than b and will only get smaller as b increases.
+    if (count >  maxcount):
+        maxcount = count
+        maxp     = p
+
+print (maxp, maxcount)
+c2 = time.clock()                
+print (c2-c1)
