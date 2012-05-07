e1dbdfb63abc6d2e495ac2ae4c5a600a8ca46035
Sameer Gauria <sameergauria@yahoo.com>
2012-05-07 14:02:25 -0700

    40

--- 40.py
+++ 40.py
@@ -1,11 +1,31 @@
-# d1    = 1
-# d10   = 1
-# d100  =
-
 def d(n):
-    if (n <= 9):
-        return n
-    elif (n <= 9+90):
+  # Express n as i^th digit (starting from left) of the j^th number of k digits
+  i,j,k = 0,0,1
+  nn = n-1
+  while True :
+    if (nn < k*9*(10 ** (k-1))):
+      j,i = divmod(nn,k)
+      break 
+    else :
+      nn -= k*9*(10 ** (k-1))
+      k += 1
+  num = (10**(k-1)) + j
+  ns  = str(num)
+  dd  = int(ns[i])
+  return dd
+
+#for i in xrange(1,200):
+#  print i,d(i)
+
+prod = 1
+for i in range(7):
+  digit = d(10**i)
+  prod *= digit
+  #print digit
+
+#print "======"
+print prod
+
         
 
                 
