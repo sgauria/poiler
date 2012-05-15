10cfac0dbe4d0f82f7cd2c17f97e3eeacbd582c1
Sameer Gauria <sameergauria@yahoo.com>
2012-05-14 17:55:32 -0700

    44:wip

--- 44.py
+++ 44.py
@@ -1,15 +1,20 @@
 N = 2000
-pns = [n*(3*n-1)/2 for n in xrange(1,N+1)]
+pns = [n*(3*n-1)/2 for n in xrange(1,2*N+1)] 
 print "pns calculated."
+#print pns
 
-for step in xrange(1,N/2):
-  print "step=",step
-  for i in xrange(N/2):
-    j = i + step
-    if j < N:
-      #if (pns[i]+pns[j]) in pns and (pns[j]-pns[i]) in pns:
-      if (pns[i]+pns[j]) in pns:
-	print pns[i], pns[j]
-	exit(0)
+s = 1 # sum
+while True :
+  for x in [2*(s-1), 2*(s-1)+1]:
+    pns.append(x*(3*x-1)/2)
+  print "sum=",s
+  for a in xrange(1,s/2):
+    b = s - a 
+    pa = pns[a]
+    pb = pns[b]
+    if (pb - pa) in pns and (pb + pa) in pns:
+      print pa, pb
+      exit(0)
+  s += 1
 
 # Unfinished code.
