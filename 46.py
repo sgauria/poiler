35b5031ed66065b280305dc47dd3f00eea65b8ea
Sameer Gauria <sameergauria@yahoo.com>
2012-05-15 17:42:09 -0700

    46 solved

--- 46.py
+++ 46.py
@@ -1,4 +1,5 @@
-from poiler_common import sieve_of_N
+from poiler_common import seive_of_E
+from math import sqrt
 
 N = 1000000
 is_prime = seive_of_E(N)
@@ -13,11 +14,13 @@ for i in xrange(3,N,2):
     odd_composites.append(i)
 
 for oc in odd_composites:
-  for j in xrange(1,oc-1):
+  meets_conjecture = 0
+  for j in xrange(1,int(sqrt(oc/2))+2):
     pp = oc - 2*j*j # possible prime
-    if is_prime[pp]:
+    if pp > 0 and is_prime[pp]:
+      meets_conjecture = 1
       break
-  if (j == oc-2):
+  if (meets_conjecture == 0):
     print (oc)
     break
 
