5e67839baceca171a06f899d3d7bef9bf4ca5b32
Sameer Gauria <sameergauria@yahoo.com>
2012-06-05 09:09:35 -0700

    comments

--- array_partition.py
+++ array_partition.py
@@ -26,9 +26,17 @@ x = []
 for i in range(N):
   x.append(random.randint(0,5000))
 
+# x = [51,49,8,7,6] # Small exampled
+
 y,z = array_partition(x)
 
 for i in x,y,z:
   print len(i), sum(i), i
   print "\n"
+
+# End-of-story.
+# Found this :
+# http://en.wikipedia.org/wiki/Partition_problem 
+# It covers it in pretty good detail.
+# I am glad I looked it up, because I don't think I was going to hit upon any of those (few) cleverer ideas. :)
 
