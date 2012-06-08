4620b205846c3d2778a83b952074b1fe63bade6a
Sameer Gauria <sameergauria@yahoo.com>
2012-06-07 22:52:10 -0700

    100 : Seeing some light

--- 100.py
+++ 100.py
@@ -1,4 +1,4 @@
-from math import sqrt, ceil
+from math import sqrt, ceil, floor
 
 root2 = sqrt(2)
 
@@ -8,15 +8,16 @@ root2 = sqrt(2)
 # Since b/n > b-1/n-1, and both must be pretty close to 1/root2 :
 # b must be ceil(n / root2)
 
-n = 10
-#n = 1000000000000 # 10^12
+b = 2
+bp=np=1
 
 print "blue,\tred,\ttotal"
 while True :
-  b = int(ceil(n/root2))
-  #print (2*b*(b-1)), (n*(n-1))
+  n = int(floor(b*root2))
   if (2*b*(b-1)) == (n*(n-1)):
-    print b, (n-b), n, (2*b*(b-1)), (n*(n-1))
-    if n > 1e12:
+    print b, n, (1.0*b/bp), (1.0*n/np)
+    bp = b
+    np = n
+    if n > 1000000:
       break 
-  n += 1
+  b += 1
