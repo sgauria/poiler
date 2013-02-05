9bb26c70df0f2ffc5a55fbe373d16cdbc35557ff
Sameer Gauria <sameergauria@yahoo.com>
2013-02-05 13:59:16 -0800

    Clean up 58

--- 58.py
+++ 58.py
@@ -1,24 +1,19 @@
-# Copied from 28. Calculate only the diagonal entries.
+# Copied from 28, and modified.
+# Calculate only the diagonal entries.
 from poiler_common import is_prime
 
-d = [1]
+prev_n = 1
 step = 2
-i = 0
 prime_count, diagonal_count = 0,1
 while True:
   for j in xrange(4):
-    n = d[4*i+j] + step
-    d.append(n)
+    n = prev_n + step
     if is_prime(n):
       prime_count += 1
+    prev_n = n
   diagonal_count += 4
-  i += 1
   step += 2
   if prime_count * 10 < diagonal_count:
     break
 
-#print d
-#print sum(d)
-
-print 2*i+1
-
+print step-1
